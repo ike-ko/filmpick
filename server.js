2d7c6dcc8a6f72231f0104a81dac6feb1b0568f3
Ko Iketani <iketani.ko@gmail.com>
2020-03-20 21:58:26 -0700

    feat: restructure server directory

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const PORT = 4000;
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -26,6 +28,12 @@ connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
 
-app.listen(PORT, function() {
-    console.log(`Server is running on Port: ${PORT}`);
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, function() {
+    console.log(`Server is running on Port: ${port}`);
 });
